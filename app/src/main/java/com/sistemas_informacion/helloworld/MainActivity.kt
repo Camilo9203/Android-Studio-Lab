0d21c7f71192aca299fde08c34ccf4fc85fba009
Camilo Ríos Hernandez <camilorios92@hotmail.com>
2023-01-24 01:01:04 -0500

    S03CL09
    Sesión 3 clase 9 evento click

--- app/src/main/java/com/sistemas_informacion/helloworld/MainActivity.kt
+++ app/src/main/java/com/sistemas_informacion/helloworld/MainActivity.kt
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
         Log.i("LOGCICLO", "Entramos en método onCreate")
+        
+        val btnLogin: Button = findViewById(R.id.buttonLogin)
+        btnLogin.setOnClickListener{view-> 
+            Toast.makeText(this, "Click en el botón login", Toast.LENGTH_SHORT).show()   
+        }
+        
+        val textLogin: TextView = findViewById(R.id.textTitle)
+        textLogin.setOnClickListener(this)
+        
+        
     }
 
     override fun onStart() {
@@ -44,9 +56,13 @@ class MainActivity : AppCompatActivity() {
         
         when(view.id) {
             R.id.buttonLogin -> {
-                Toast.makeText(this, "Ingresando", Toast.LENGTH_LONG).show()
+
             }
         }
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "Click en titulo", Toast.LENGTH_SHORT).show()
     }
 
 }
